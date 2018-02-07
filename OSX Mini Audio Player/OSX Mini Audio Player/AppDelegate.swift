2f838c75382f54abfaa71875ffa180e3904553f4
Satendra Dagar <satendra.techmac@gmail.com>
2018-02-07 12:55:15 +0530

    Added initial project

---
+++ OSX Mini Audio Player/OSX Mini Audio Player/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSX Mini Audio Player
+//
+//  Created by Satendra Dagar on 07/02/18.
+//  Copyright © 2018 CB. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSX Mini Audio Player/OSX Mini Audio Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSX Mini Audio Player
+//
+//  Created by Satendra Dagar on 07/02/18.
+//  Copyright © 2018 CB. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
